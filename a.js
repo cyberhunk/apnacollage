d64cd936c214bda2bf2b6f97f0f07dc42d0ef4f2
delta student <satya.pk0011@gmail.com>
2023-11-10 15:02:19 +0530

    first commit

---
+++ a.js
@@ -0,0 +1,15 @@
+// function jod(a,b){
+//     return a + b ;
+// }
+//  let a = 4;
+//  let b= 5;
+//  console.log(jod(a,b));
+
+// EVENT HANDLING
+
+const butt = document.getElementById('click');
+
+butt.addEventListener('click' , function(){
+    alert('submited');
+
+});
